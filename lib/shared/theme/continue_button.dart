c1f7f28ae50b2ffa2f015aff03044c743b0a767e
Tanguy Mossion <tanguymossion@orange.fr>
2024-02-19 18:38:59 +0100

    feat: create ContinueButton

---
+++ lib/shared/theme/continue_button.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class ContinueButton extends StatelessWidget {
+  const ContinueButton({super.key, required this.onPressed});
+
+  final void Function() onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onPressed,
+      child: Container(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 36,
+          vertical: 10,
+        ),
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: SvgPicture.asset('assets/svg/right_arrow.svg'),
+      ),
+    );
+  }
+}
