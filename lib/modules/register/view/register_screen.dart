7e02c7687f96fb461b82edb17b8cde4a0dc83c0a
Scuym <doussetsylvain@gmail.com>
2024-02-24 13:45:23 +0100

    :fire: sign in with google and print user information

--- lib/modules/register/view/register_screen.dart
+++ lib/modules/register/view/register_screen.dart
@@ -1,19 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:photofiesta/shared/theme/colors.dart';
 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: AppColors.yellowBackground,
       body: SafeArea(
         child: Center(
-          child: Text('Register',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+          child: ElevatedButton(
+            onPressed: () => signInWithGoogle(),
+            child: const Text('Sign in with Google'),
+          ),
         ),
       ),
     );
+  }
+
+  Future<void> signInWithGoogle() async {
+    try {
+      final GoogleSignInAccount? account = await GoogleSignIn(
+              /* needed for web
+              clientId: 'firebase-projet-id'
+              */
+              )
+          .signIn();
+      if (account != null) {
+        final GoogleSignInAuthentication googleAuth =
+            await account.authentication;
+        final credential = GoogleAuthProvider.credential(
+          accessToken: googleAuth.accessToken,
+          idToken: googleAuth.idToken,
+        );
+        await FirebaseAuth.instance.signInWithCredential(credential);
+        await saveUser(account);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> saveUser(GoogleSignInAccount account) async {
+    print(account.displayName);
+    print(account.email);
+    print(account.photoUrl);
   }
 }
