f3024b3a3bea7480c010b85516cd407b2ab6a73e
Tanguy Mossion <tanguymossion@orange.fr>
2024-02-29 22:36:36 +0100

    feat: add needed colors

--- lib/shared/theme/colors.dart
+++ lib/shared/theme/colors.dart
@@ -2,4 +2,12 @@ import 'package:flutter/material.dart';
 
 abstract final class AppColors {
   static const Color yellowBackground = Color(0xFFFFE298);
+  static const Color yellowBubble = Color(0xFFFFEEC3);
+
+  static const Color pinkGradientStart = Color(0xFFED719A);
+  static const Color pinkGradientEnd = Color(0xFFEB896E);
+  static const Color blueGradientStart = Color(0xFF4F77ED);
+  static const Color blueGradientEnd = Color(0xFF0FB4FC);
+
+  static const Color greyText = Color(0xFF545454);
 }
