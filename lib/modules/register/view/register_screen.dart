fb6d26ffa94dff38808a1f1f276da1559174e26f
Scuym <doussetsylvain@gmail.com>
2024-03-01 14:56:49 +0100

    user exist check, creation and redirect

--- lib/modules/register/view/register_screen.dart
+++ lib/modules/register/view/register_screen.dart
@@ -1,8 +1,11 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 import 'package:photofiesta/shared/theme/colors.dart';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({super.key});
@@ -14,7 +17,7 @@ class RegisterScreen extends StatelessWidget {
       body: SafeArea(
         child: Center(
           child: ElevatedButton(
-            onPressed: () => signInWithGoogle(),
+            onPressed: () => signInWithGoogle(context),
             child: const Text('Sign in with Google'),
           ),
         ),
@@ -22,14 +25,10 @@ class RegisterScreen extends StatelessWidget {
     );
   }
 
-  Future<void> signInWithGoogle() async {
+  Future<void> signInWithGoogle(BuildContext context) async {
     try {
-      final GoogleSignInAccount? account = await GoogleSignIn(
-              /* needed for web
-              clientId: 'firebase-projet-id'
-              */
-              )
-          .signIn();
+      final GoogleSignInAccount? account =
+          await GoogleSignIn(/*clientId: ''*/).signIn();
       if (account != null) {
         final GoogleSignInAuthentication googleAuth =
             await account.authentication;
@@ -38,16 +37,40 @@ class RegisterScreen extends StatelessWidget {
           idToken: googleAuth.idToken,
         );
         await FirebaseAuth.instance.signInWithCredential(credential);
-        await saveUser(account);
+        if (context.mounted) {
+          await saveUser(context, account);
+        }
       }
     } catch (e) {
       print(e);
     }
   }
 
-  Future<void> saveUser(GoogleSignInAccount account) async {
-    print(account.displayName);
-    print(account.email);
-    print(account.photoUrl);
+  Future<void> saveUser(
+      BuildContext context, GoogleSignInAccount account) async {
+    final userCollection = FirebaseFirestore.instance.collection('user');
+    final userDoc = userCollection.doc(account.id);
+
+    final userSnapshot = await userDoc.get();
+
+    // Si l'utilisateur existe, go homepage
+    if (userSnapshot.exists) {
+      if (context.mounted) {
+        Navigator.of(context).pushReplacementNamed('homepage');
+      }
+    }
+    // S'il n'existe pas, le créer
+    else {
+      await userDoc.set({
+        'displayName': account.displayName,
+        'email': account.email,
+        'photoUrl': account.photoUrl,
+      });
+
+      // Rediriger vers la page pseudo
+      if (context.mounted) {
+        Navigator.of(context).pushReplacementNamed('pseudo');
+      }
+    }
   }
 }
